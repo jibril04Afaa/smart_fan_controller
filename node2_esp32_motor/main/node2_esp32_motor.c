2874e8c7d2cb08b7b52c6c91b5ef073950304de4
jibril04Afaa <jibrilBA04@gmail.com>
2026-07-18 06:47:51 -0700

    ESP32 setup and FreeRTOS hello world loop

--- node2_esp32_motor/main/node2_esp32_motor.c
+++ node2_esp32_motor/main/node2_esp32_motor.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
-
+    while (1)
+    {
+        printf("Hello from ESP32 node...! \n");
+        vTaskDelay(1000/portTICK_PERIOD_MS); // 1000 ms delay cos why not
+    }
+    
 }
