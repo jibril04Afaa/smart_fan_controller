23a8d76b0ce347aec8b9383683bb4a9d4b2e16ff
jibril04Afaa <jibrilBA04@gmail.com>
2026-07-22 22:30:49 -0700

    define

--- node2_esp32_motor/main/node2_esp32_motor.c
+++ node2_esp32_motor/main/node2_esp32_motor.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+
+// define UART pins
+#define UART_PORT_NUM UART_NUM_2 // (USART2 in stm32cubeIDE)
+#define UART_TX 17 // GPIOP17
+#define UART_RX 16 // GPIOP16
+#define UART_BAUD_RATE 115200 // UART must be the same baud rate
+#define UART_BUFSIZE 1024
 
 void app_main(void)
 {
